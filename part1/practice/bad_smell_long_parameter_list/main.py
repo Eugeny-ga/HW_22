0ff560203a90880bf04ad6bb7c1a572ffdc5ecc8
Eugeny-ga <118772991+Eugeny-ga@users.noreply.github.com>
2023-05-17 19:49:52 +0300

    v 1.1

--- part1/practice/bad_smell_long_parameter_list/main.py
+++ part1/practice/bad_smell_long_parameter_list/main.py
@@ -11,35 +11,36 @@
 
 
 class Unit:
-    def __init__(self, is_fly, crawl, speed=1):
-        self.is_fly = is_fly
-        self.crawl = crawl
-        self.speed = speed
-        if self.is_fly and self.crawl:
-            raise ValueError('Рожденный ползать летать не должен!')
 
-
-    def move(self, x_coord, y_coord, direction):
-        speed = self._get_speed()
-        if direction == 'UP':
-            y_coord += speed
-        elif direction == 'DOWN':
-            y_coord -= speed
-        elif direction == 'LEFT':
-            x_coord -= speed
-        elif direction == 'RIGTH':
-            x_coord += speed
-
-        return (x_coord, y_coord)
-
-
+    def __init__(self, field, x_coord, y_coord, state: str = None):
+        self.speed = 1
+        self.state = state
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+        self.field = field
 
     def _get_speed(self):
-        if self.is_fly:
-            self.speed *= 1.2
+
+        if not self.state:
+            return self.speed
+        elif self.state == 'fly':
+            return self.speed * 1.2
+        elif self.state == 'crawl':
+            return self.speed * 0.5
         else:
-            self.speed *= 0.5
-        return self.speed
+            raise ValueError('Ошибка состояния')
+
+    def move(self, direction):
+        speed = self._get_speed()
+
+        if direction == 'UP':
+            self.field.set_unit(self.x_coord, self.y_coord + speed, unit=self)
+        elif direction == 'DOWN':
+            self.field.set_unit(self.x_coord, self.y_coord - speed, unit=self)
+        elif direction == 'LEFT':
+            self.field.set_unit(self.x_coord - speed, self.y_coord, unit=self)
+        elif direction == 'RIGTH':
+            self.field.set_unit(self.x_coord + speed, self.y_coord, unit=self)
 
 
 #     ...
